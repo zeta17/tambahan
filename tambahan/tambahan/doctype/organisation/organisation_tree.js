b976166e3c20c6c7f639deac9bf1ae5f239f15e4
zeta17 <hendrik.zeta@gmail.com>
2017-10-31 00:20:57 -0700

    tree_2

--- tambahan/tambahan/doctype/organisation/organisation_tree.js
+++ tambahan/tambahan/doctype/organisation/organisation_tree.js
@@ -1,3 +1,8 @@
 frappe.treeview_settings["Organisation"] = {
   ignore_fields:["parent_organisation"],
+  onrender: function(node) {
+		$('<span class="balance-area pull-right text-muted small">'
+			+ " " + node.data.value
+			+ '</span>').insertBefore(node.$ul);
+	},
 }
